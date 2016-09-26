20d3a126034890ba889550e523713cb9e01a9eb0
Michael Musick <michael@michaelmusick.com>
2016-09-26 09:56:39 -0600

    Added reading

--- p5_Examples/05_objects_arrays/05_objects_push_new/sketch.js
+++ p5_Examples/05_objects_arrays/05_objects_push_new/sketch.js
@@ -3,21 +3,21 @@
 // http://www.learningprocessing.com
 
 // Example 9-11 ported to p5.js
-// Resizing an array is JS is as easy as "push()"
+// Resizing an array in JS is as easy as "push()"
 
 var balls = []; // We start with an empty array
 var gravity = 0.1;
 
 function setup() {
   createCanvas(640,360);
-  
+
   // Initialize ball index 0
   balls.push(new Ball(50,0,24));
 }
 
 function draw() {
   background(51);
-  
+
   // Update and display all balls
   for (var i = 0; i < balls.length; i ++ ) { // Whatever the length of that array, update and display all of the objects.
     balls[i].gravity();
@@ -29,6 +29,6 @@ function draw() {
 function mousePressed() {
   // A new ball object
   var b = new Ball(mouseX,mouseY,24); // Make a new object at the mouse location.
-  // Here, the function push() adds an element to the end of the array. 
+  // Here, the function push() adds an element to the end of the array.
   balls.push(b);
 }
