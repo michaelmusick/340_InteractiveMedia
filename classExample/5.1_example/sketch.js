c17d143c6165b8c98a43f405e8411379ab9f6b7e
Michael Musick <michael@michaelmusick.com>
2016-09-26 14:19:38 -0600

    In class object example of a Sad Face

---
+++ classExample/5.1_example/sketch.js
@@ -0,0 +1,68 @@
+var sadFace = {
+	faceDiam: 300,
+	posX: 400,
+	posY: 200,
+	eyeY: 0,
+	eyeX: 0,
+	eyeXDiam: 0,
+	eyeYDiam: 0,
+	mouthX: 0,
+	mouthY: 0,
+	mouthWidth: 0,
+	mouthHeight: 0,
+	mouthOffset: 0,
+
+	defineFace: function(){
+		this.eyeY = 0 - this.faceDiam * (1/6);
+		this.eyeX = this.faceDiam * (1/6);
+		this.eyeXDiam = this.faceDiam/10;
+		this.eyeYDiam = this.faceDiam/5;
+		this.mouthY = this.faceDiam * (1/6);
+		this.mouthWidth = this.faceDiam * (2/3);
+		this.mouthHeight = this.faceDiam * (0.2);
+	},
+
+	drawFace: function(){
+		stroke(0);
+		// Sad face drawing
+		translate(this.posX,this.posY);
+		// Face
+		ellipse(0,0,this.faceDiam,this.faceDiam);
+		ellipse(-this.eyeX,this.eyeY,this.eyeXDiam,this.eyeYDiam);
+		ellipse(this.eyeX,this.eyeY,this.eyeXDiam,this.eyeYDiam);
+		arc(this.mouthX, this.mouthY, this.mouthWidth, this.mouthHeight, PI + this.mouthOffset, 0 + this.mouthOffset);
+		arc(this.mouthX, this.mouthY, this.mouthWidth, this.mouthHeight * (0.5), PI + this.mouthOffset, 0 + this.mouthOffset);
+
+
+
+	},
+
+	move: function(){
+		this.posX += 1;
+	},
+
+	flipMouth: function() {
+		this.mouthOffset = (this.mouthOffset + PI) % TWO_PI;
+	}
+}
+
+
+
+
+
+function setup() {
+	createCanvas(800,400);
+	sadFace.defineFace();
+}
+
+function draw() {
+	background(0);
+
+	sadFace.drawFace();
+	sadFace.move();
+
+}
+
+function mousePressed(){
+	sadFace.flipMouth();
+}
--- p5_Examples/00_empty_example/sketch.js
+++ p5_Examples/00_empty_example/sketch.js
@@ -1,7 +1,38 @@
+var circleheight = 2
+var circlewidth = 2
+var col = {
+  r:255,
+  g:255,
+  b:255
+}
 function setup() {
-  // put setup code here
+  createCanvas(600,400);
+  background(0);
+  fill(255);
+  text("HOLD BUTTON TO RECEIVE ART",205,190)
+
 }
 
 function draw() {
-  // put draw code here
-}
+  rectMode(CENTER);
+  // making my button
+  rect(300,200, 4,4);
+  // defining perimeters of my very small button
+  if(mouseX >296 & mouseX < 304 & mouseY > 196 & mouseY < 204){
+  // if my mouse is within the perimeter of my button then the statement is true which allows my second if statement to work
+  if(mouseIsPressed){
+    //randomizing the rgb colors of my growing ellipse
+    col.r = random(0,255);
+    col.g = random(0,255);
+    col.b = random(0,255);
+    stroke(col.r, col.g, col.b);
+    // ellipse appears where my mouse is
+    ellipse(mouseX,mouseY,circlewidth,circleheight);
+    noFill(1);
+    // my ellipse's diameter will grow continuosly by 1 pixel
+    circleheight = circleheight + 1
+    circlewidth = circlewidth + 1
+
+  }
+  }
+  }
